29731f4d1c17e54b5c83a9e5ab31ea92175e4a1a
brunatb <brutopanb@gmail.com>
2021-01-19 22:09:51 -0300

    Get product informations by id

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,7 +1,19 @@
+const NotFoundError = require("../errors/NotFoundError");
+const Categorie = require("../models/Categorie")
+const Photo = require("../models/Photo")
 const Product = require("../models/Product")
 
 async function getProductInformations(id){
+   const product = Product.findOne({
+       where: {
+           id,
+       },
+       include: [Categorie, Photo],
+   });
    
+   if(!product) throw new NotFoundError;
+
+   return product;
 }
 
 module.exports = {
--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const ProductNotFoundError = require('../errors/ProductNotFoundError');
-
 const router = express.Router();
+
+const NotFoundError = require('../errors/NotFoundError');
+const productsController = require('../controllers/productsController');
 
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
     try{
-
+        return productsController.getProductInformations(id);
     }catch(err){
-        if(err instanceof ProductNotFoundError){
+        if(err instanceof NotFoundError){
             res.status(404).send({ error: "Produto não encontrado!" })
         }else{
             res.status(500).send({ error: 'call someone' });
