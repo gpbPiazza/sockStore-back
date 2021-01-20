6f2beaa753e08145ebb6ac673186910d3da5098e
brunatb <brutopanb@gmail.com>
2021-01-19 22:55:34 -0300

    Commit to merge

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -4,16 +4,16 @@ const ProductNotFoundError = require('../errors/ProductNotFoundError');
 const router = express.Router();
 
 router.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    try{
+  const { id } = req.params;
+  try {
 
-    }catch(err){
-        if(err instanceof ProductNotFoundError){
-            res.status(404).send({ error: "Produto não encontrado!" })
-        }else{
-            res.status(500).send({ error: 'call someone' });
-        }
+  } catch (err) {
+    if (err instanceof ProductNotFoundError) {
+      res.status(404).send({ error: 'Produto não encontrado!' });
+    } else {
+      res.status(500).send({ error: 'call someone' });
     }
+  }
 });
 
 module.exports = router;
