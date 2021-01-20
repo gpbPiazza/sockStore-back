bfcf81b43eb7e7f02185838c53fae42c073bb1b1
brunatb <brutopanb@gmail.com>
2021-01-20 16:09:53 -0300

    Get categorized products route

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,6 +1,8 @@
 const Category = require('../models/Category');
 const ConflictError = require('../errors/ConflictError');
 const NotFoundError = require('../errors/NotFoundError');
+const Product = require('../models/Product');
+const Photo = require('../models/Photo');
 
 async function create({ name }) {
   const findCategoryByName = await Category.findOne({ where: { name } });
@@ -25,9 +27,30 @@ function count() {
   return Category.count();
 }
 
+async function getCategorizedProducts() {
+  const categorizedProducts = await Category.findAll({
+    include: [{
+      model: Product,
+      Order: [['stock', 'id', 'DES']],
+      attributes: ['id', 'name', 'price', 'size', 'description', 'stock'],
+      include: [{
+        model: Photo,
+        attributes: ['id', 'photo'],
+      }],
+      through: {
+        attributes: [],
+      },
+    }],
+    attributes: ['id', 'name'],
+  });
+
+  return categorizedProducts;
+}
+
 module.exports = {
   create,
   getAll,
   deleteCategory,
   count,
+  getCategorizedProducts,
 };
