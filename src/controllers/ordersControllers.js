e4186c7c0b0ac6d42664083226507fef8bdd1838
Gabriel Pedro B Piazza <pedro.piazza99@gmail.com>
2021-01-21 20:03:55 -0300

    commit merge

--- src/controllers/ordersControllers.js
+++ src/controllers/ordersControllers.js
@@ -1,5 +1,7 @@
 const Client = require('../models/Address');
 const Address = require('../models/Address');
+const Product = require('../models/Product');
+const Order = require('../models/Order');
 
 async function postOrder(client, address, products) {
   const postedClient = await Client.create({ client });
@@ -9,6 +11,34 @@ async function postOrder(client, address, products) {
   }
 }
 
+function getAllOrders() {
+  return Order.findAll({
+    model: Client,
+    include: [{
+      model: Product,
+      attributes: ['id', 'name', 'description', 'stock'],
+    }],
+
+  });
+}
+
+// include: [{
+//   model: Category,
+//   attributes: ['id', 'name'],
+//   through: {
+//     model: CategoriesProduct,
+//     attributes: [],
+//   },
+// },
+// {
+//   model: Photo,
+//   attributes: ['id', 'photo'],
+// }],
+// offset,
+// limit,
+// }
+
 module.exports = {
   postOrder,
+  getAllOrders,
 };
