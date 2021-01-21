a659e1426ed5e3c0630bb40cbb50919836c2d745
Gabriel Pedro B Piazza <pedro.piazza99@gmail.com>
2021-01-21 18:16:43 -0300

    add ssl false in migration production

--- config/config.js
+++ config/config.js
@@ -11,5 +11,12 @@ module.exports = {
   },
   production: {
     url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
